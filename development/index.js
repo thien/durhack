35deda5a0babe4e4c574173f5150d3f5ff44e42e
@thien <thien@users.noreply.github.com>
2018-07-20 23:57:23 +0100

    changed port to be consistent with nginx configuration

--- development/index.js
+++ development/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8080
 
 // if this is on, redirect the user to the teaser page.
 // they shouldn't be able to see anything else.
