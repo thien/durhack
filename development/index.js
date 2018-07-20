6de8372cc87199e7725ea220ecfd36400e2b59ce
@thien <thien@users.noreply.github.com>
2018-07-20 23:38:51 +0100

    initial commit of new environment

---
+++ development/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// if this is on, redirect the user to the teaser page.
+// they shouldn't be able to see anything else.
+const showTeaser = true
+
+if (showTeaser) {
+  // show the teaser page until ready.
+  app.use("/", express.static(__dirname + '/public/teaser'));
+} else {
+  // display our WIP website.
+  app.get('/', (request, response) => {
+    response.send('Hello from Express!')
+  })
+}
+
+app.listen(port, (err) => {
+  if (err) {
+    // return response to user..
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
