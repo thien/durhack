ba47d66ea6b1b922daac0e758bd24049dbbaa770
thien <thien@users.noreply.github.com>
2018-08-23 22:11:01 +0100

    added files for initial commit of preview

--- development/index.js
+++ development/index.js
@@ -4,11 +4,15 @@ const port = 8080
 
 // if this is on, redirect the user to the teaser page.
 // they shouldn't be able to see anything else.
-const showTeaser = true
+const showTeaser = false;
+const showSplash = true;
 
 if (showTeaser) {
   // show the teaser page until ready.
   app.use("/", express.static(__dirname + '/public/teaser'));
+} else if (showSplash) {
+  // show the teaser page until ready.
+  app.use("/", express.static(__dirname + '/public/preview'));
 } else {
   // display our WIP website.
   app.get('/', (request, response) => {
