c1a35ba043bb38e649d79858bf8e6efa12ac0b75
Robbie Jakob-Whitworth <mail@rjwhitworth.co.uk>
2019-05-02 00:31:11 +0100

    Use npm for some dependencies

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -45,8 +45,8 @@ module.exports = (env, argv) => {
             files: {
                 "vendor.js": [
                     path.resolve(__dirname, './assets/vendor/js/auto-complete.js'),
-                    path.resolve(__dirname, './assets/vendor/js/particles.min.js'),
-                    path.resolve(__dirname, './assets/vendor/js/typer.js')
+                    path.resolve(__dirname, './node_modules/particles.js/particles.js'),
+                    path.resolve(__dirname, './node_modules/typer-js/typer.min.js')
                 ]
             }
         })
